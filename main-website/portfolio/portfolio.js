2a2709826951893d0e00b5ce8a2781cc79bc1be4
ruster-mw <mateuszwypior2000@gmail.com>
2025-01-14 07:14:58 +0000

    Delete files from repository

--- main-website/portfolio/portfolio.js
+++
@@ -1,7 +0,0 @@
-function setVw() {
-    let vw = document.documentElement.clientWidth / 100;
-    document.documentElement.style.setProperty('--vw', `${vw}px`);
-  }
-  
-  setVw();
-  window.addEventListener('resize', setVw);
