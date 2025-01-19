d913cb14852cb8ec740626b40e9883ecf0754353
ruster-mw <mateuszwypior2000@gmail.com>
2025-01-19 17:44:38 +0100

    Add files via upload

--- todo.js
+++ todo.js
@@ -9,6 +9,7 @@ let learn = document.querySelector("#learn")
 let tasks = document.querySelectorAll(".task")
 let taskNames = document.querySelectorAll(".taskname")
 let searchForm = document.querySelector(".searchForm")
+let storedNotes = []
 // ----------------------------------------------------------------------------------------------------------
 function showNoteSection() {
     noteSection.style.display = "block";
@@ -32,26 +33,54 @@ function statusColor(statusDiv){
         }
     })
 }
-// function statusColor(){
-//     let statusdivs = document.querySelectorAll(".statusdiv")
-//     statusdivs.forEach(statusdiv => {
-//         statusdiv.addEventListener('click', function(){
-//             let child = statusdiv.firstChild
-//             let parent = statusdiv.parentElement
-//             console.log(parent)
-//             if(statusdiv.style.backgroundColor == 'red'){
-//                statusdiv.style.backgroundColor = "lime"
-//                child.style.clipPath = "polygon(28% 38%, 41% 53%, 75% 24%, 86% 38%, 40% 78%, 15% 50%)"
-//                parent.style.textDecoration = "line-through"
-//             }
-//             else{
-//                statusdiv.style.backgroundColor ='red'
-//                child.style.clipPath = "polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%)"
-//                parent.style.textDecoration = "none"
-//             }
-//         })
-//     })
-//     }
+function saveNoteToLocalStorage(){
+    let texts = document.querySelectorAll(".taskname")
+    texts.forEach(item => {
+        storedNotes.push(item.textContent)
+    })
+    localStorage.setItem("storedNotes",JSON.stringify(storedNotes))
+}
+function createSavedNotes(){
+    const ur_data_noob = JSON.parse(localStorage.getItem("storedNotes"))
+    ur_data_noob.forEach(item => {
+    let li = document.createElement("LI")
+    li.className = "task"
+    let statusdiv = document.createElement("div")
+    statusdiv.className = "statusdiv"
+    let statussign = document.createElement("div")
+    statussign.className = "statussign"
+    let taskname = document.createElement("span")
+    taskname.className = "taskname"
+    let deletebtn = document.createElement("button")
+    deletebtn.className = "delete"
+    let xsign = document.createElement("div")
+    xsign.className = "X"
+    let value = item
+    taskname.innerText = value
+    statusdiv.appendChild(statussign)
+    deletebtn.appendChild(xsign)
+    li.appendChild(statusdiv)
+    li.appendChild(taskname)
+    li.appendChild(deletebtn)
+    console.log(statusdiv)
+    console.log(li)
+    container.appendChild(li)
+    deletebtn.addEventListener('click', function() {
+        let parent = deletebtn.parentElement;
+        parent.style.width = "0";
+        let ur_data_noob2 = JSON.parse(localStorage.getItem("storedNotes"))
+        setTimeout(() => {
+            parent.remove();
+            let new_data = ur_data_noob2.filter(item => item != taskname.innerText)
+            console.log(new_data)
+            localStorage.setItem("storedNotes",JSON.stringify(new_data))
+        }, 500);
+    });
+
+    statusColor(statusdiv)
+    addInput.value = ""
+    })
+}
 function addNote(){
     let li = document.createElement("LI")
     li.className = "task"
@@ -75,7 +104,19 @@ function addNote(){
     console.log(statusdiv)
     console.log(li)
     container.appendChild(li)
-    removeButton()
+    saveNoteToLocalStorage()
+    deletebtn.addEventListener('click', function() {
+        let parent = deletebtn.parentElement;
+        parent.style.width = "0";
+        let ur_data_noob2 = JSON.parse(localStorage.getItem("storedNotes"))
+        setTimeout(() => {
+            parent.remove();
+            let new_data = ur_data_noob2.filter(item => item != taskname.innerText)
+            console.log(new_data)
+            localStorage.setItem("storedNotes",JSON.stringify(new_data))
+        }, 500);
+    });
+
     statusColor(statusdiv)
     addInput.value = ""
 }
@@ -117,8 +158,8 @@ searchForm.addEventListener('keyup', function(e){
 });
 
 
-
-removeButton()
+createSavedNotes()
+// removeButton()
 statusColor(dishes)
 statusColor(conquer)
 statusColor(learn)
